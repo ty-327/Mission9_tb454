bceb03c7dde9b3198b8b9cbe1945fff3d16d04ca
ty-327 <tyisb7@gmail.com>
2023-03-16 01:25:04 -0600

    final commit for mission 11 i think

---
+++ Mission9_tb454/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission9_tb454.Models;
+namespace Mission9_tb454.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
